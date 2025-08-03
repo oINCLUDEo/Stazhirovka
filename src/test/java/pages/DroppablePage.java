237a44588b14d33f769340594ee17b800b6f58cb
Nikita Zhuklin <nikita-zhuklin@rambler.ru>
2025-08-03 19:03:51 +0400

    grammar update

--- src/test/java/pages/DroppablePage.java
+++ src/test/java/pages/DroppablePage.java
@@ -6,7 +6,6 @@ import org.openqa.selenium.support.FindBy;
 
 import java.util.function.Supplier;
 
-import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.*;
 
